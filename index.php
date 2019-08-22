21bf75d8f678df2b6fca5a01741aac83a1e0872f
lucasan746 <cariverp9@gmail.com>
2019-08-22 14:47:33 -0300

    :)

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php echo "Hola ;)";
-echo "adios";
-echo "cusbai";
-echo "adios";
-?>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!-- Menu de registro -->
+    <section class="cajalogin">
+      <article class="artilogin">
+        <h1 class="titulog">Lorem ipsum</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat..</p>
+        <a class="botonlog" href="">Registrarse</a>
+        <a class="botonlog2" href="">Iniciar Sesión</a>
+      </article>
+    </section>
+    <!-- fin :c -->
+  </body>
+</html>
