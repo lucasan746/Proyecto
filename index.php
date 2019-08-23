b32f037ea85a441a2474e07c0425a49ba7d946d6
lucasan746 <cariverp9@gmail.com>
2019-08-23 17:58:44 -0300

    hola

--- index.php
+++ index.php
@@ -20,6 +20,10 @@
     <!-- fin :c -->
       <!-- Preguntas y respuestas -->
 <section class="preg1">
+  <header class="barramenu">
+    <a href="#" class="botonlog3">Inicia Sesión</a>
+    <a href="#" class="botonlog4">Registrate</a>
+  </header>
   <h1 class="titulo1">Pregunta</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
