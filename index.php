ae0da583db5689b59ebb168a6c5a99396c7c0e33
FatimaRodriguez <fatimarodriguezc@hotmail.com>
2019-08-23 21:44:24 -0300

    actualizacion fati

--- index.php
+++ index.php
@@ -25,19 +25,24 @@
     <!-- fin :c -->
       <!-- Preguntas y respuestas -->
 <section class="preg1">
+  <article class="contenido1">
+    <img src="images/molan1.jpeg" alt="molang1">
   <h1 class="titulo1">Pregunta</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
+</article>
 </section>
 <section class="preg2">
   <article class="contenido2">
+    <div class="data1">
   <h1 class="titulo2">Pregunta </h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+  <img src="images/molang.png" alt="molang">
 </article>
-<h1>pruebilla</h1>
+
 </section>
 
 <!-- fin parte fati -->
