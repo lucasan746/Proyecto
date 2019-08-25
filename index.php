912234624d26b80ab689b62e6622ae8bd59c40ff
DillanSL <965257814d@gmail.com>
2019-08-24 23:53:22 -0300

    Corregido

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
       <!-- Preguntas y respuestas -->
 <section class="preg1">
   <article class="contenido1">
-    <img src="images/molan1.jpeg" alt="molang1">
+    <img src="images/molang1.jpeg" alt="molang1">
   <h1 class="titulo1">Pregunta</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -71,7 +71,7 @@
 </div>
 <<<<<<< HEAD
 =======
-<h1>hola</h1>
+<h1>hola i</h1>
 <h2>hola</h2>
 >>>>>>> new
   </body>
