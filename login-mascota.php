bc5167e65e52590c5cb4da12da1a392acf874327
FatimaRodriguez <fatimarodriguezc@hotmail.com>
2019-08-26 21:47:36 -0300

    login mascota

---
+++ login-mascota.php
@@ -0,0 +1,19 @@
+<?php include 'images.php'; ?>
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="utf-8">
+    <title>login mascota</title>
+  </head>
+  <body>
+<section>
+  <section class="cajalogin" id="<?= $imagenes[$alea] ?>">
+    <a href="#"><img src="images/icon-lazy.png" alt="icono"  class="iconoprin"></a>
+</section>
+
+
+
+  </body>
+</html>
