a1195a45f0f0b7359200c38767fe817f19840af1
lucasan746 <cariverp9@gmail.com>
2019-08-28 19:33:29 -0300

    barra

--- navbar.php
+++ navbar.php
@@ -1,7 +1,12 @@
 <header>
-  <nav>
+  <nav class="navbar">
       <a href="index.php"><img src="images/icon-lazy.png" alt="icono"  class="iconoprin"></a>
-      <input id="submit" value="" type="submit">
-	<label for="submit" class="submit"></label>
+      <FORM method=GET action="">
+        <label for="busqueda"></label>
+        <input type="text" name="busqueda" class="busqueda">
+      </FORM>
+      <img src="images/iconos/noti.png" alt="" class="iconos">
+      <img src="images/iconos/mensajes.png" alt="" class="iconos">
+      <img src="images/iconos/perfil.jpg" alt="" class="fotoperfil">
   </nav>
 </header>
