5b644e474bee7a059ac69cba61af2516d14d8ec7
lucasan746 <cariverp9@gmail.com>
2019-08-28 15:51:55 -0300

    navbar

---
+++ navbar.php
@@ -0,0 +1,7 @@
+<header>
+  <nav>
+      <a href="index.php"><img src="images/icon-lazy.png" alt="icono"  class="iconoprin"></a>
+      <input id="submit" value="" type="submit">
+	<label for="submit" class="submit"></label>
+  </nav>
+</header>
