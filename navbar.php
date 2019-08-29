159ceffc1fc49093e04a6e5146af6381eb22ded0
lucasan746 <cariverp9@gmail.com>
2019-08-29 15:45:48 -0300

    barra

--- navbar.php
+++ navbar.php
@@ -1,12 +1,23 @@
 <header>
   <nav class="navbar">
-      <a href="index.php"><img src="images/icon-lazy.png" alt="icono"  class="iconoprin"></a>
-      <FORM method=GET action="">
-        <label for="busqueda"></label>
-        <input type="text" name="busqueda" class="busqueda">
-      </FORM>
-      <img src="images/iconos/noti.png" alt="" class="iconos">
-      <img src="images/iconos/mensajes.png" alt="" class="iconos">
-      <img src="images/iconos/perfil.jpg" alt="" class="fotoperfil">
+      <div class="iconosderecha">
+        <div class="ic">
+          <img src="images/icon-lazy.png" alt="iconoprincipal" class="iconoprin2">
+          <img src="images/iconos/usuario.png" alt="solicitudes" class="iconosbarra">
+          <img src="images/iconos/bocadillo.png" alt="mensajes" class="iconosbarra">
+          <img src="images/iconos/notificacion.png" alt="notificaciones" class="iconosbarra">
+          <img src="images/iconos/grupo.png" alt="grupos" class="iconosbarra">
+          <img src="images/iconos/ajustes.png" alt="ajustes" class="iconosbarra">
+        </div>
+        <div class="barrabus">
+          <form class="" action="#" method="get">
+            <input type="text" name="busqueda" class="input" placeholder="Busqueda">
+            
+          </form>
+        </div>
+      </div>
+      <div class="perfil">
+
+      </div>
   </nav>
 </header>
