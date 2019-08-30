50da5be8ef7948e43db25812241176b9e0c3206a
lucasan746 <cariverp9@gmail.com>
2019-08-30 12:01:12 -0300

    modificaciones barra

--- login.php
+++ login.php
@@ -11,16 +11,16 @@
       <a href="index.php"><img src="images/icon-lazy.png" alt="icono"  class="iconoprin"></a>
       <article class="login">
         <h1 class="titulog">Sloth</h1>
-    
+
         <form class="iniciases" action="home.php" method="post">
           <label for="email"></label>
           <input type="text" name="email" placeholder="Email o Nombre de usuario">
           <label for="Contraseña"></label>
           <input type="text" name="contraseña" placeholder="Contraseña">
           <button type="submit" name="enviar" class="botonlog">Iniciar Sesión</button>
-          <a class="botonlog2" href="registro.php">Crea una cuenta</a>
         </form>
       </article>
+      <a class="botoninic" href="registro.php">Registrate</a>
     </section>
   </body>
 </html>
