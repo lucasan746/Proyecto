2624fab6df5b2ab850dfec776664aab84ad5ea9a
DillanSL <965257814d@gmail.com>
2019-08-31 23:24:24 -0300

    footer secc 2

--- seccion2.php
+++ seccion2.php
@@ -1,28 +1,35 @@
 <section class="seccionder">
   <article class="sugeridos">
-    <h1>Sugeridos</h1>
+    <h1>Sugeridos para ti</h1>
     <ul>
-      <img class="img" src="images/home/iconom.jpg" alt="icono"> <a href="user@user.com">Lechuga Verde</a>
+      <img class="img" src="images/home/iconom.jpg" alt="icono"> <a href="user@user.com">Lechuga_verde</a>
       <img class="img" src="images/home/iconom1.jpg" alt="icono">
-      <a href="user@user.com">Gato Miau</a>
+      <a href="user@user.com">Satan.21</a>
       <img class="img" src="images/home/iconom2.jpg" alt="icono">
-      <a href="user@user.com">Perro Guau</a>
+      <a href="user@user.com">Copito_Hs</a>
       <img class="img" src="images/home/iconom3.jpg" alt="icono">
-      <a href="user@user.com">Papa Rellena</a>
+      <a href="user@user.com">Bolita.000</a>
       <img class="img" src="images/home/iconom.jpg" alt="icono">
-      <a href="user@user.com">Lechuga Verde</a>
+      <a href="user@user.com">Lechuga_verde</a>
       <img class="img" src="images/home/iconom1.jpg" alt="icono">
-      <a href="user@user.com">Gato Miau</a>
+      <a href="user@user.com">Satan.21</a>
       <img class="img" src="images/home/iconom2.jpg" alt="icono">
-      <a href="user@user.com">Perro Guau</a>
+      <a href="user@user.com">Copito_Hs</a>
       <img class="img" src="images/home/iconom3.jpg" alt="icono">
-      <a href="user@user.com">Papa Rellena</a>
+      <a href="user@user.com">Bolita.000</a>
     </ul>
   </article>
 
   <article class="adopciones">
     <img src="images/home/adopcion.jpg" alt="fondo" width="100%" height="100%">
-    <!-- <h2>Páginas de adopciones</h2> -->
     <button type="submit" name="adoptar">Adoptar</button>
   </article>
+  <footer>
+    <ul class="foothome">
+      <li><a href="#">Contacto</a></li>
+      <li><a href="#">Legales</a></li>
+      <li><a href="#">Ayuda</a></li>
+      <li><a href="#">Privacidad</a></li>
+    </ul>
+  </footer>
 </section>
