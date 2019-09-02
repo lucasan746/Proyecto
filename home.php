c0e48e5dd5fe79ead306ca6f9a67f6ef03fdf6c5
lucas746 <cariverp9@gmail.com>
2019-09-02 02:53:03 -0300

    responsive home

--- home.php
+++ home.php
@@ -2,6 +2,8 @@
 <html lang="es" dir="ltr">
   <head>
     <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport"
+    content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <title></title>
   </head>
@@ -11,6 +13,5 @@
     <?php require_once 'seccion1.php'; ?>
     <?php require_once 'seccion2.php'; ?>
   </section>
-
   </body>
 </html>
