b9d395dd4e66e99a1aedc210404a13c95a030409
lucasan746 <cariverp9@gmail.com>
2019-09-01 21:35:46 -0300

    responsive

--- registro2.php
+++ registro2.php
@@ -23,60 +23,46 @@ $pais = [
     <title>Registro</title>
   </head>
   <body id="<?= $imagenes[$alea] ?>">
-    <section class="cajalogin">
-      <a href="#"><img src="images/icon-lazy.png" alt="icono"  class="iconoprin"></a>
-        <a class="botoninic" href="home.php">Iniciar sesión</a>
-      <br>
-    <article class="cajareg">
-      <div class="cajatitulo">
-        <h1 class="titulog">Sloth</h1>
-        <h3>Un espacio para tu mascota</h3>
-      </div>
-      <div class="formulario">
-        <div class="formulario1">
-      <form class="registro" action="registro-mascota.php" method="post">
-          <br>
+    <a href="#"><img src="images/icon-lazy.png" alt="icono"  class="iconoprin"></a>
+      <a class="botoninic" href="home.php">Iniciar sesión</a>
+    <section class="secreg">
+      <article class="formulario">
+        <form class="" action="index.html" method="post">
+          <div style="width:50%; float:left;">
           <label for="nombre">Nombre:</label>
           <input class="reg1" type="text" name="nombre">
-          <br><br>
           <label for="apellido">Apellido:</label>
           <input class="reg1" type="text" name="apellido">
-          <br><br>
           <label for="confcontra">Nombre de Usuario:</label>
           <input class="reg1" type="text" name="usuario">
-          <br><br>
           <label for="email">E-mail:</label>
           <input class="reg1" type="text" name="email">
-          <br><br>
-        </form>
         </div>
-        <div class="formulario2">
-          <form class="registro" action="registro-mascota.php">
-          <br>
+        <div style="width:50%; float:left;">
           <label class="f2" for="contraseña">Contraseña:</label>
           <input class="reg1" type="password" name="conraseña">
-          <br><br>
           <label class="f2"for="confcontra">Confirmar contraseña:</label>
           <input class="reg1" type="password" name="confcontra">
-          <br><br>
           <label class="f2" for="sexo">Género:</label>
           <input class="genero" type="radio" name="sexo" value="M">Mujer
           <input class="genero" type="radio" name="sexo" value="H">Hombre
           <input class="genero" type="radio" name="sexo" value="O">Otro
           <label class="f2" for="date">Fecha:</label>
           <input class="date" type="date" name="fecha">
-          <br><br>
           <label class="f2" for="pais">País:</label>
           <select class="" name="pais">
             <?php foreach ($pais as $codigo => $pais): ?>
               <option value=<?=$codigo?>><?=$pais?></option>
             <?php endforeach; ?>
           </select>
-          <br><br>
           <button type="submit" name="siguiente" class="botonreg">Siguiente</button>
         </div>
-      </form>
-      </div>
-    </article>
+        </form>
+
+      </article>
+    </section>
+
+
+
   </body>
 </html>
