33675947de7a71b1eba927772e6d72b732a75fd3
lucasan764 <cariverp9@gmail.com>
2019-09-25 17:36:19 -0300

    modificaiones

--- registro.php
+++ registro.php
@@ -12,19 +12,19 @@ $pais = [
 ];
 include_once 'validacion.php';
 if ($_POST) {
-  $errores=validar($_POST);
-  $user=ValirdarUser($_POST);
-  var_dump($user);
-  if ($user==false) {
-  if ($errores==0) {
-    $FotoDePerfil=fotoPerfil($_FILES["fotoperfil"]);
-    $usuario=armarUsuario($_POST,$FotoDePerfil);
-    guardarUsuario($usuario);
+  $errores=validar($_POST,$_FILES);
+  if ($errores!=0) {
+    foreach ($errores as $key => $value) {
+      echo "<p class='error'>* $value";
+    }
   }
-}}
-// if ($user==true) {
-//   echo "el email esta registrado";
-// }
+  else {
+    $imagendeperfil=fotoPerfil($_FILES);
+    $usuarionew=armarUsuario($_POST,$imagendeperfil);
+  }
+  }
+  $ListaUsuarios=BaseDeDatos();
+
 ?>
 
 <?php include 'images.php'; ?>
@@ -54,22 +54,46 @@ if ($_POST) {
 
         <form class="registro" action="registro.php" method="post" enctype="multipart/form-data">
 
-          <input class="reg1" type="text" placeholder="Nombre" name="nombre">
+          <input class="reg1" type="text" placeholder="<?php if (isset($errores["nombre"])) {
+            echo $errores["nombre"];
+          } else {
+            echo "Nombre";
+          }?>" name="nombre">
           <br>
 
-          <input class="reg2" type="text" placeholder="Apellido" name="apellido">
+          <input class="reg2" type="text" placeholder="<?php if (isset($errores["apellido"])) {
+            echo $errores["apellido"];
+          } else {
+            echo "Apellido";
+          }?>" name="apellido">
           <br>
 
-          <input class="reg3" type="text" placeholder="Nombre de usuario" name="usuario">
+          <input class="reg3" type="text" placeholder="<?php if (isset($errores["usuario"])) {
+            echo $errores["usuario"];
+          } else {
+            echo "Nombre de usuario";
+          }?>" name="usuario">
           <br>
 
-          <input class="reg4" type="text"placeholder="Correo electrónico" name="email">
+          <input class="reg4" type="text"placeholder="<?php if (isset($errores["email"])) {
+            echo $errores["email"];
+          } else {
+            echo "Correo electronico";
+          }?>" name="email"
           <br>
 
-          <input class="reg5" type="password" placeholder="Contraseña" name="contraseña">
+          <input class="reg5" type="password" placeholder="<?php if (isset($errores["contraseña"])) {
+            echo $errores["contraseña"];
+          } else {
+            echo "Contraseña";
+          }?>" name="contraseña">
           <br>
 
-          <input class="reg6" type="password" placeholder="Confirmar contraseña"name="confcontra">
+          <input class="reg6" type="password" placeholder="<?php if (isset($errores["confcontra"])) {
+            echo $errores["confcontra"];
+          } else {
+            echo "Confirmar Contraseña";
+          }?>"name="confcontra">
           <br>
 
           <input class="gen1" type="radio" name="sexo" value="M">
@@ -81,18 +105,18 @@ if ($_POST) {
           <br>
           <select class="regday" name="dia">
             <option selected value="0">Dia</option>
-            <option value="">1</option>
-            <option value="">2</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
           </select>
           <select class="regmonth" name="mes">
             <option selected value="">Mes</option>
-            <option value="">Enero</option>
-            <option value="">Febrero</option>
+            <option value="Enero">Enero</option>
+            <option value="Febrero">Febrero</option>
           </select>
           <select class="regyear" name="año">
             <option selected value="">Año</option>
-            <option value="">1950</option>
-            <option value="">2006</option>
+            <option value="1950">1950</option>
+            <option value="2006">2006</option>
 
           </select>
           <br>
--- validacion.php
+++ validacion.php
@@ -1,23 +1,34 @@
 <?php
-function validar($datos){
+function validar($datos,$imagen){
       $errores=[];
-      if (strlen($_POST["nombre"])==0) {
-        $errores= "El campo no puede estar vacio";
+      if (strlen($datos["nombre"])==0) {
+        $errores["nombre"]= "El nombre no puede estar vacio";
       }
-      if (strlen($_POST["apellido"])==0) {
-        $errores= "El campo no puede estar vacio";
+      if (strlen($datos["apellido"])==0) {
+        $errores["apellido"]= "El apellido no puede estar vacio";
       }
-      if (strlen($_POST["usuario"])==0) {
-        $errores= "El campo no puede estar vacio";
+      if (strlen($datos["usuario"])==0) {
+        $errores["usuario"]= "El nombre de usuario no puede estar vacio";
       }
       if (filter_var($datos["email"],FILTER_VALIDATE_EMAIL)) {
-        $errores= "El email es incorrecto";
+        $errores["email"] = "El email es incorrecto";
       }
       if (strlen($datos["contraseña"])<=6) {
-        $errores= "La contraseña debe tener minimo 6 caracteres";
+        $errores["contraseña"]= "La contraseña debe tener minimo 6 caracteres";
       }
-      if ($_POST["contraseña"]!=$_POST["confcontra"]) {
-        $errores= "Las contraseñas no coinciden";
+      if ($datos["contraseña"]!=$datos["confcontra"]) {
+        $errores["confcontra"]= "Las contraseñas no coinciden";
+      }
+      if ($datos["dia"]==null||$datos["mes"]==null||$datos["año"]==null) {
+        $errores["fecha"]= "Debes seleccionar una fecha de Nacimiento";
+      }
+      if ($imagen["fotoperfil"]["error"]!=0) {
+        $errores["imagen"]="Error en la imagen";
+      }
+      $nombimg = $_FILES["fotoperfil"]["name"];
+      $ext = pathinfo($nombimg, PATHINFO_EXTENSION);
+      if ($ext != "jpg" && $ext != "jpeg" && $ext != "png") {
+        $errores["fotoperfil"] = "La extension del archivo es incorrecto";
       }
       return $errores;
     }
@@ -76,4 +87,5 @@ function armarUsuario($datos,$imagen)
      }
    }
  }
+
 ?>
