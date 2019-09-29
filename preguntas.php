ccb326d2905eb80eaff5bc314fb378d3222bb550
DillanSL <965257814d@gmail.com>
2019-09-29 17:58:23 -0300

    Preguntas

---
+++ preguntas.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title></title>
+  </head>
+  <body>
+    <section class="preguntas">
+    <a href="index.php"><img src="images/icon-lazy.png" alt="icono"  class="iconoprin"></a>
+      <article class="faq">
+        <h1>Crea tu slot</h1>
+        <h1>Conoce gente como tú</h1>
+        <h1>Ayuda a otros animales</h1>
+        <h1>Unete a Sloth</h1>
+      </article>
+      <article class="faq1">
+        <p>Dale voz a tu mascota. Crea tu perfil o el de tu mascota y cuenta cómo es su vida, cuáles son sus gustos o qué lugares han visitado.
+      Encuentra amigos y compañeros de juegos para tu mascota y haz actividades en común con ellos.</p>
+        <p>¿Quieres encontrar a gente que comparta tu misma pasión por los animales? En Sloth puedes participar en grupos con tus mismos intereses, crear tus propias comunidades, organizar quedadas y eventos.</p>
+        <p>Consulta las necesidades de las protectoras, apúntante a programas de voluntariado, haz donaciones... ¡haz que este mundo sea un poco más feliz para los animales que sufren!</p>
+        <p>Sumate a todos aquellos amantes de los animales que quieran compartir y disfrutar al máximo de la experiencia de tener una mascota.
+  Te esperamos dentro!.</p>
+      </article>
+    </section>
+
+  </body>
+</html>
