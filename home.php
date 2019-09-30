d4a8b2f6bace0599440de8f3c85725ce43baa5de
lucasan764 <cariverp9@gmail.com>
2019-09-30 00:38:09 -0300

    responsive

--- home.php
+++ home.php
@@ -19,7 +19,20 @@ content="width=device-width, initial-scale=1">
     <?php require_once 'navbar.php'; ?>
     <section class="cuerpohome">
       <section class="barratablet">
-        <img src="<?php echo "fotos/".$_SESSION["FotoDePerfil"]; ?>" alt="fotoperfil" class="fotoperfil">
+        <nav class="navbar2">
+            <div class="iconosderecha">
+
+                <img src="images/iconos/usuario.png" alt="solicitudes" class="icon-solicitud">
+                <img src="images/iconos/bocadillo.png" alt="mensajes" class="icon-mensaje">
+                <img src="images/iconos/notificacion.png" alt="notificaciones" class="icon-notif">
+                <img src="images/iconos/ajustes.png" alt="grupos" class="icon-config">
+                <img src="<?php echo "fotos/".$_SESSION["FotoDePerfil"]; ?>" alt="fotoperfil" class="icon-foto">
+                <a href="finsesion.php"><img src="images/iconos/outblanco.png" alt="ajustes" class="icon-cierre"></a>
+
+
+
+            </div>
+        </nav>
       </section>
     <?php require_once 'seccion1.php'; ?>
     <?php require_once 'seccion2.php'; ?>
